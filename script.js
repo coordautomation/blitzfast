175351c9ea88df1df21d06b21e85440f206d9a1f
coordautomation <78256951+coordautomation@users.noreply.github.com>
2024-05-10 16:01:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document
+  .querySelector(".inquiry-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    var formData = new FormData(this); // Gather form data
+
+    fetch(
+      "https://script.google.com/macros/s/AKfycbxCGB8VIkB30tnWo-eINarVIYWzPzoCkLdP9KWTAqXpi-LOvB10gHDqkIPg1XXrYmVbnw/exec",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((response) => response.text())
+      .then((text) => {
+        alert(
+          "We have received your inquiry, we will get back to you shortly on the provided email address."
+        );
+        document.querySelector(".inquiry-form").reset(); // Reset form after submission
+      })
+      .catch((error) => console.error("Error:", error));
+  });
