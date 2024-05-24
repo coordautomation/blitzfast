e600eeba78209b7ec183782b12308592113fe356
coordautomation <78256951+coordautomation@users.noreply.github.com>
2024-05-24 21:46:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-document
-  .querySelector(".inquiry-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
+const form = document.querySelector("#form")
+   const submitButton = document.querySelector("#submit")
+   const scriptURL =  "https://script.google.com/macros/s/AKfycbwe2qjhdh7cMlAlVNqg3UPNalnP3v9h10aXpqPpQHnmPScPsmx8UmFYAY3qkk7E0rGPLA/exec"
 
-    var formData = new FormData(this); // Gather form data
 
-    fetch(
-       "https://script.google.com/macros/s/AKfycbxCGB8VIkB30tnWo-eINarVIYWzPzoCkLdP9KWTAqXpi-LOvB10gHDqkIPg1XXrYmVbnw/exec"
-      
-           {
-        method: "POST",
-        body: formData,
-      }
-    )
-      .then((response) => response.text())
-      .then((text) => {
-        alert(
-          "We have received your inquiry, we will get back to you shortly on the provided email address."
-        );
-        document.querySelector(".inquiry-form").reset(); // Reset form after submission
-      })
-      .catch((error) => console.error("Error:", error));
-  });
+   form.addEventListener('submit', e => {
+     submitButton.disabled = true
+     e.preventDefault()
+     let requestBody = new FormData(form)
+     fetch(scriptURL, { method: 'POST', body: requestBody})
+       .then(response => {
+          alert('Success!', response)
+          submitButton.disabled = false
+         })
+       .catch(error => {
+       alert('Error!', error.message)
+         submitButton.disabled = false
+
+       }
+       )
+   })
+</script>
