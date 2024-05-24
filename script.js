428577fc0bbf8096f90510825a23ad3b3e64f553
coordautomation <78256951+coordautomation@users.noreply.github.com>
2024-05-24 22:03:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const form = document.querySelector(".inquiry-form")
-   const submitButton = document.querySelector("#submit")
-   const scriptURL =  "https://script.google.com/macros/s/AKfycbwe2qjhdh7cMlAlVNqg3UPNalnP3v9h10aXpqPpQHnmPScPsmx8UmFYAY3qkk7E0rGPLA/exec"
+document
+  .querySelector(".inquiry-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
 
+    var formData = new FormData(this); // Gather form data
 
-   form.addEventListener('submit', e => {
-     submitButton.disabled = true
-     e.preventDefault()
-     let requestBody = new FormData(form)
-     fetch(scriptURL, { method: 'POST', body: requestBody})
-       .then(response => {
-          alert('Success!', response)
-          submitButton.disabled = false
-         })
-       .catch(error => {
-       alert('Error!', error.message)
-         submitButton.disabled = false
+    fetch(
+       " https://script.google.com/macros/s/AKfycbwe2qjhdh7cMlAlVNqg3UPNalnP3v9h10aXpqPpQHnmPScPsmx8UmFYAY3qkk7E0rGPLA/exec"
 
-       }
-       )
-   })
-</script>
+            
+           {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((response) => response.text())
+      .then((text) => {
+        alert(
+          "We have received your inquiry, we will get back to you shortly on the provided email address."
+        );
+        document.querySelector(".inquiry-form").reset(); // Reset form after submission
+      })
+      .catch((error) => console.error("Error:", error));
+  });
