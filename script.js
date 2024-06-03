46b50203c6ddaf91e0bb67b0f20cc6d0e744210c
coordautomation <78256951+coordautomation@users.noreply.github.com>
2024-06-03 18:28:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-document.querySelector(".inquiry-form").addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent the default form submission
+document
+  .querySelector(".inquiry-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+               
+    var formData = new FormData(this); // Gather form data
 
-  var formData = new FormData(this); // Gather form data
-
-  fetch("https://script.google.com/macros/s/AKfycbw9vUx_4Aqw-Fxvw8KeENzlwSCkANWUk4fP0LkydqmhgUzF7izhaa_ukEjQ4iyvNqVc-g/exec", {
-    method: "POST",
-    body: formData,
-  })
-  .then((response) => response.text())
-  .then((text) => {
-    alert("We have received your inquiry, we will get back to you shortly on the provided email address.");
-    document.querySelector(".inquiry-form").reset(); // Reset form after submission
-  })
-  .catch((error) => console.error("Error:", error));
-});
+    fetch(
+       "https://script.google.com/macros/s/AKfycbw9vUx_4Aqw-Fxvw8KeENzlwSCkANWUk4fP0LkydqmhgUzF7izhaa_ukEjQ4iyvNqVc-g/exec"
+            
+           {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((response) => response.text())
+      .then((text) => {
+        alert(
+          "We have received your inquiry, we will get back to you shortly on the provided email address."
+        );
+        document.querySelector(".inquiry-form").reset(); // Reset form after submission
+      })
+      .catch((error) => console.error("Error:", error));
+  });
 
       
