b692948dec6cc06cbddbb8b4fa21a89be6a9d16a
coordautomation <78256951+coordautomation@users.noreply.github.com>
2024-06-03 16:16:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-document
-  .querySelector(".inquiry-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission
-               
-    var formData = new FormData(this); // Gather form data
+function doPost(e) {
+  var sheet = SpreadsheetApp.openById('YOUR_SHEET_ID').getSheetByName('Sheet1');
+  var data = JSON.parse(e.postData.contents);
 
-    fetch(
-       "https://script.google.com/macros/s/AKfycbw9vUx_4Aqw-Fxvw8KeENzlwSCkANWUk4fP0LkydqmhgUzF7izhaa_ukEjQ4iyvNqVc-g/exec"
-            
-           {
-        method: "POST",
-        body: formData,
-      }
-    )
-      .then((response) => response.text())
-      .then((text) => {
-        alert(
-          "We have received your inquiry, we will get back to you shortly on the provided email address."
-        );
-        document.querySelector(".inquiry-form").reset(); // Reset form after submission
-      })
-      .catch((error) => console.error("Error:", error));
-  });
+  sheet.appendRow([data.name, data.email, data.message]);
+  
+  return ContentService.createTextOutput('Success');
+}
+
+function doGet(e) {
+  return ContentService.createTextOutput('GET request received.');
+}
